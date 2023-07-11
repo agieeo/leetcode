0661d959a5f1dbb3c5e7a5a30234e580e655e946
agieeo <kanamemadokaw@gmail.com>
2023-07-11 14:14:23 +0800

    两种解法

--- 两数之和.java
+++ 两数之和.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {//暴力解法
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length; //C++里用nums.size()
         for (int i=0; i<n-1;++i){
@@ -7,6 +7,18 @@ class Solution {
                     return new int[]{i,j};//C++:return {i,j}
                 }
             }
+        }
+        return new int[]{};
+    }
+}
+class Solution2 { //hash解法
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();//map为空
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{i,map.get(target-nums[i])};
+            }
+            map.put(nums[i],i);//这个将nums[i],放入map的不能放前面
         }
         return new int[]{};
     }
